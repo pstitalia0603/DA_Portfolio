d357593246068af1ea8803c15f42079aa0ba4866
PSF <48135781+pstitalia0603@users.noreply.github.com>
2021-05-29 13:41:21 -0400

    Add files via upload

--- Tableau Portfolio Project SQL Queries.sql
+++ Tableau Portfolio Project SQL Queries.sql
@@ -1,5 +1,7 @@
 /*
+
 Queries used for Tableau Project
+
 */
 
 
@@ -7,7 +9,8 @@ Queries used for Tableau Project
 -- 1. 
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From LinuxDB..[owid-covid-deaths] --Where location like '%states%'
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
 where continent is not null 
 --Group By date
 order by 1,2
@@ -30,7 +33,7 @@ order by 1,2
 -- European Union is part of Europe
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
-From LinuxDB..[owid-covid-deaths]
+From PortfolioProject..CovidDeaths
 --Where location like '%states%'
 Where continent is null 
 and location not in ('World', 'European Union', 'International')
@@ -41,7 +44,7 @@ order by TotalDeathCount desc
 -- 3.
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From LinuxDB..[owid-covid-deaths]
+From PortfolioProject..CovidDeaths
 --Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
@@ -51,10 +54,11 @@ order by PercentPopulationInfected desc
 
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From LinuxDB..[owid-covid-deaths]
+From PortfolioProject..CovidDeaths
 --Where location like '%states%'
 Group by Location, Population, date
 order by PercentPopulationInfected desc
+
 
 
 
@@ -175,3 +179,7 @@ From PortfolioProject..CovidDeaths
 --Where location like '%states%'
 Group by Location, Population, date
 order by PercentPopulationInfected desc
+
+
+
+
